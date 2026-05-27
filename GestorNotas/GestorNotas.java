408ce8dcde0a2f92f8aa1133a3ce2a9f5750a356
Nicole <nicole.pevas@gmail.com>
2026-05-27 20:10:32 +0200

    Implementado método crearNota en GestorNotas

--- GestorNotas/GestorNotas.java
+++ GestorNotas/GestorNotas.java
@@ -9,9 +9,11 @@ public class GestorNotas {
         this.notas = new ArrayList<>();
     }
 
-    // TODO: implementar alta de notas
     public void crearNota(String titulo, String contenido, boolean importante) {
-        // implementar en la fase correspondiente
+        int id = notas.size() + 1;
+        Nota nota = new Nota(id, titulo, contenido, importante);
+        notas.add(nota);
+        System.out.println("Nota creada con id: " + id);
     }
 
     // TODO: implementar listado
